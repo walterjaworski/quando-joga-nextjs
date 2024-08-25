5469fa8edbac9752cfacae8545f9fbe764a2a1a5
Walter Jaworski <walter.jaworski@gmail.com>
2024-08-25 12:47:26 -0300

    change paths

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -1,5 +1,5 @@
-import escudo from "@/public/images/escudo-athletico.svg";
 import Image from "next/image";
+import escudo from "../../../public/images/escudo-athletico.svg";
 
 export default function HeaderLogo() {
   return (
